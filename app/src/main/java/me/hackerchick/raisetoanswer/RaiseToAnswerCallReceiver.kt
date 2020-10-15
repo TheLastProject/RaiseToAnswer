0b33ec3de4cdaa875ef368cad0cf103a19265fd6
Sylvia van Os <sylvia@hackerchick.me>
2020-10-15 21:24:00 +0200

    Ignore incoming call when already in a call

--- app/src/main/java/me/hackerchick/raisetoanswer/RaiseToAnswerCallReceiver.kt
+++ app/src/main/java/me/hackerchick/raisetoanswer/RaiseToAnswerCallReceiver.kt
@@ -4,13 +4,18 @@ import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.telephony.TelephonyManager
+
 
 class RaiseToAnswerCallReceiver : BroadcastReceiver() {
     @SuppressLint("MissingPermission", "NewApi")
     override fun onReceive(context: Context, intent: Intent?) {
         val state = intent!!.getStringExtra(TelephonyManager.EXTRA_STATE)
-        if (TelephonyManager.EXTRA_STATE_RINGING == state) {
+
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        if (TelephonyManager.EXTRA_STATE_RINGING == state && audioManager.mode != AudioManager.MODE_IN_CALL) {
             Util.startSensorListener(context, false)
         } else {
             Util.stopSensorListener(context)
